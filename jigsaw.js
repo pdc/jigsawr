88e5cce92af97cbea185101632cd0044757ecfbe
pdc <pdc@alleged.org.uk>
2010-08-14 19:11:29 +0100

    Puzzle pieces now snap together when brought close enough.
    The snapped-together pieces comprise one 'chunk' and are
    now dragged about as a unit.
    
    When snapping together, the dragged chunk moves enough to make
    the connection seamless.
    
    The result is an actually playable jigsaw!

--- jigsaw.js
+++ jigsaw.js
@@ -6,7 +6,7 @@ var doc = null;
 var rootElt = null;
 
 // Find an element by ID.
-function elt(idOrElt) {
+function getElt(idOrElt) {
     if (idOrElt.getAttribute) {
         return idOrElt;
     }
@@ -42,7 +42,7 @@ var logMax = 600 / 16;
 
 function log(text) {
     if (logTspans == null) {
-        logElt = subelt(elt('bg'), 'text', {
+        logElt = subelt(getElt('bg'), 'text', {
             'font-height': logLineHt,
             x: 0,
             y: logLineHt
@@ -68,15 +68,6 @@ function log(text) {
 // Application specific
 //
 
-// Called from main SVG element when document loaded.
-function init(evt) {
-	doc = evt.target.ownerDocument;
-	rootElt = doc.documentElement;
-	
-	var pieces = mkPieces('im.jpg', 640, 427, 4, 4);
-	initDrag('p', pieces);
-};
-
 //
 // hv -- either 'h' for a bottom or top side or 'v'
 // vh -- either 'v' or 'h'
@@ -87,24 +78,23 @@ function bump(hv, vh, len, bwh, bhw) {
     return [hv + 0.5 * (len - bwh), vh + bhw, hv + bwh, vh + -bhw, hv + 0.5 * (len - bwh)];
 }
 
-function mkPieces(u, imWd, imHt, nh, nv) {	
+function mkPieceElts(u, imWd, imHt, nh, nv) {	
     var wd = imWd / nh;
     var ht = imHt / nv;
-	var pieces = [];
-	var defsElt = elt('defs');
-	var piecesElt = elt('p');
-	var dwd = piecesElt.ownerSVGElement.width.animVal.value;
-	var dht = piecesElt.ownerSVGElement.height.animVal.value;
-	var fracWd = 1 / nh;
-	var fracHt = 1 / nv;
-	
+	var hunks = [];
+	var defsElt = getElt('defs');
+	var hunksElt = getElt('p');
+	var dwd = hunksElt.ownerSVGElement.width.animVal.value;
+	var dht = hunksElt.ownerSVGElement.height.animVal.value;
 	
     var hash = function (i, j) {
         return ((i * 37) ^ (j * 1009)) % 17;
     }
-
 	
+	var pieceEltss = []; // 2-d array of hunks
+	var pieceElts = []; // simple list of hunks
 	for (var i = 0; i < nh; ++i) {
+	    pieceEltss[i] = []
 	    for (var j = 0; j < nv; ++j) {
 		    var id = String.fromCharCode(97 + i) + String.fromCharCode(97 + j);
 		    
@@ -167,11 +157,15 @@ function mkPieces(u, imWd, imHt, nh, nv) {
 	        // Random starting position.
 		    var x = Math.random() * (dwd - wd);
 		    var y = Math.random() * (dht - ht);
+		    
+		    /*
+		    // Non-random starting points to see that the hunks fit.
 		    x = i * wd * 1.15;
 		    y = j * ht * 1.15;
+		    */
 		    
 		    // Create image with clip path.
-		    var pieceElt = subelt(piecesElt, 'g', {
+		    var pieceElt = subelt(hunksElt, 'g', {
 		        id: 'g' + id,
 		        transform: 'translate(' + x + ',' + y + ')'	        
 		    }) 
@@ -183,21 +177,46 @@ function mkPieces(u, imWd, imHt, nh, nv) {
 	            fill: 'url(#p' + id + ')'
 	        })
 	        
-            pieceElt.ownerPiece = {elts: {id: pieceElt}};
-		    pieces.push(pieceElt.ownerPiece);
+		    pieceEltss[i][j] = pieceElt;
+		    pieceElts.push(pieceElt)
 	    }
 	}
-	return pieces;
+	
+	for (i = 0; i < nh; ++i) {
+	    for (j = 0; j < nv; ++j) {
+	        pieceEltss[i][j].adjs = []
+	        if (i > 0) {
+	            pieceEltss[i][j].adjs.push({elt: pieceEltss[i - 1][j], dx: -wd});
+            }
+            if (i + 1 < nh) {
+	            pieceEltss[i][j].adjs.push({elt: pieceEltss[i + 1][j], dx: +wd});
+            }
+            if (j > 0) {
+	            pieceEltss[i][j].adjs.push({elt: pieceEltss[i][j - 1], dy: -ht});
+            }
+            if (j + 1 < nv) {
+	            pieceEltss[i][j].adjs.push({elt: pieceEltss[i][j + 1], dy: +ht});
+            }
+	    }
+	}
+	
+	return pieceElts;
 }
 
-function initDrag(bgElt, pieces) {
-    bgElt = elt(bgElt);
+function initDrag(bgElt, pieceElts) {
+    bgElt = getElt(bgElt);
     var isDrag = false;
-    var dragPiece = null;
+    var dragHunk = null;
     var dragStartX = null;
     var dragStartY = null;
     var startX = null;
     var startY = null;
+    
+    
+    var hunks = [];
+    for (var i in pieceElts) {
+        hunks.push(pieceElts[i].hunk = {elts: [pieceElts[i]]});
+    }
 
     // SVGPoint to keep track of the offset of the dragging session
     var dragOffset = rootElt.createSVGPoint();
@@ -207,8 +226,8 @@ function initDrag(bgElt, pieces) {
             var shiftX = evt.clientX - dragStartX;
             var shiftY = evt.clientY - dragStartY;
             
-            for (var j in dragPiece.elts) {
-                var e = dragPiece.elts[j];
+            for (var j in dragHunk.elts) {
+                var e = dragHunk.elts[j];
                 var t = 'translate(' + (e.startX + shiftX) + ',' + (e.startY + shiftY) + ')';
                 e.setAttribute('transform', t);
             
@@ -224,24 +243,65 @@ function initDrag(bgElt, pieces) {
             dragElt.style.setProperty('pointer-events', 'all');
             bgElt.style.setProperty('pointer-events', 'none');
             */
-            dragPiece = null;
+            
+            // Is it close enough to a piece to snap together?
+            var snaps = [];
+            var checkedElts = {};
+            for (var i in dragHunk.elts) {
+                var e = dragHunk.elts[i];
+                var m = e.getCTM();
+                var dragX = m.e;
+                var dragY = m.f;
+                for (var j in e.adjs) {
+                    var adj = e.adjs[j];
+                    if (adj.elt.hunk == dragHunk || checkedElts[adj.elt]) {
+                        continue;
+                    }
+                    var m = adj.elt.getCTM();
+                    var otherX = m.e - (adj.dx || 0);
+                    var otherY = m.f - (adj.dy || 0);
+                    var dSquared = Math.pow(dragX - otherX, 2) + Math.pow(dragY - otherY, 2);
+                    log('dragX,Y=' + dragX + ',' + dragY + '; otherX,Y=' + otherX + ',' + otherY
+                        + '; dSquared=' + dSquared);
+                    if (dSquared < 25) {
+                        log('snap');
+                        snaps.push({elts: adj.elt.hunk.elts, dx: otherX - dragX, dy: otherY - dragY});
+                        checkedElts[adj.elt] = true;
+                    }
+                }
+            }
+            for (var i in snaps) {
+                var snap = snaps[i];
+                for (var k in dragHunk.elts) {
+                    var e = dragHunk.elts[k]
+                    var m = e.getCTM();
+                    var t = 'translate(' + (m.e + snap.dx) + ',' + (m.f + snap.dy) + ')';
+                    e.setAttribute('transform', t);
+                }
+                for (var k in snap.elts) {
+                    snap.elts[k].hunk = dragHunk;
+                }
+                dragHunk.elts = dragHunk.elts.concat(snap.elts);
+            }
+            log('Hunk size: ' + dragHunk.elts.length);
+            
+            dragHunk = null;
             isDrag = false;
-            log('dun; dragPiece=' + typeof(dragPiece) + ', isDrag=' + isDrag);
         }
     }, false);
 
-    for (var pieceIndex in pieces) {
-        var piece = pieces[pieceIndex];
-        for (var eltID in piece.elts) {
-            var pieceElt = piece.elts[eltID];
+    for (var k in hunks) {
+        var hunk = hunks[k];
+        for (var eltID in hunk.elts) {
+            var pieceElt = hunk.elts[eltID];
             pieceElt.addEventListener('mousedown', function (evt) {
-                dragPiece = evt.currentTarget.ownerPiece;
+                dragHunk = evt.currentTarget.hunk;
                 isDrag = true;
 
                 dragStartX = evt.clientX;
                 dragStartY = evt.clientY;
-                for (var j in dragPiece.elts) {
-                    var e = dragPiece.elts[j];
+                for (var j in dragHunk.elts) {
+                    var e = dragHunk.elts[j];
                     var m = e.getCTM();
                     e.startX = m.e;
                     e.startY = m.f;
@@ -256,3 +316,12 @@ function initDrag(bgElt, pieces) {
         }
     }
 }
+
+// Called from main SVG element when document loaded.
+function init(evt) {
+	doc = evt.target.ownerDocument;
+	rootElt = doc.documentElement;
+	
+	var pieceElts = mkPieceElts('im.jpg', 640, 427, 6, 4);
+	initDrag('p', pieceElts);
+};
