5e67c80ce16ec79a43fc67bf3dd45893f11d7551
pdc <pdc@alleged.org.uk>
2010-08-22 23:26:01 +0100

    Refactor the 2 forms in to 1 form.
    The picture URL or Flickr tags are shown by clicking on a link
    in the form.

--- index.js
+++ index.js
@@ -25,61 +25,6 @@ $(document).ready(function () {
         e$.change(function () {updateSubmit($(this))});
     }
 */
-
-    var getJigsawSize = function () {
-        var em = $('#c a').height(); // SO TEMPTING to just write 13 here and be done with it.
-        return {
-            width: $(document).width(),
-            height: $(document).height() - 6 * em
-        };
-    }
-    
-    // this is the form that creates the jigsaw
-    $('#f').submit(function (evt) {
-        $(this).addClass('l');
-        var jigSize = getJigsawSize();
-        var jigWd = jigSize.width, jigHt = jigSize.height;
-        var imSrc = $('#u').val();
-        var im = $('<img>').attr('src', imSrc);
-        imElt = im.get(0);
-        var isLoaded = false;
-        im.load(function () {
-            if (!isLoaded) { 
-                var wd = imElt.width;
-                var ht = imElt.height;
-                if (wd > jigWd) {
-                    ht *= jigWd / wd;
-                    wd = jigWd;
-                }
-                if (ht > jigHt) {
-                    wd *= jigHt / ht;
-                    ht = jigHt;
-                }
-                var n = $('#d').val();
-                var args = {
-                    u: imSrc,
-                    wd: wd,
-                    ht: ht,
-                    nh: Math.ceil(Math.pow(n * wd / ht, .5)),
-                    nv: Math.ceil(Math.pow(n * ht / wd, .5))
-                }
-                var embed = $('<embed>').attr({
-                    src: 'jigsaw.svg' + escapeArgs(args),
-                    type: 'image/svg+xml',
-                    width: jigWd,
-                    height: jigHt
-                });
-                $('#x').empty().append(embed);
-                $('body').addClass('j');
-                $('#f').removeClass('l');
-                isLoaded = true;
-            }
-        });
-        if (imElt.complete) {
-            im.trigger('load');
-        }
-        evt.preventDefault();
-    });
     
     var apiKey = '489c9667c5c8957340a78bacacb051d6';
     var flickrCall = function (meth, args, func) {
@@ -97,52 +42,109 @@ $(document).ready(function () {
         })
     };
     
-    // This is the form that updates the image URL.
-    $('#g').submit(function (evt) {
-        $(this).addClass('l');
-        var tags = $('#t').val();
-        var perPage = 25;
-        flickrCall('photos.search', {
-            tags: tags.split().join(','),
-            license: '1,2,4,5,7',
-            sort: 'interestingness-desc',
-            content_type: 1,
-            media: 'photos',
-            per_page: perPage,
-            extras: 'owner_name'
-        }, function (data) {
-            // Choose a photo.
-            var photoIndex = Math.floor(Math.random() * perPage);
-            var photo = data.photos.photo[photoIndex];
-            // Now to find the size of the photo that fits best.
-            flickrCall('photos.getSizes', {photo_id: photo.id}, function (data) {
-                // Loop over sizes to get best fit.
-                var jigSize = getJigsawSize();
-                var jigWd = jigSize.width, jigHt = jigSize.height;        
-                var prev;
-                for (var i = 0; i < data.sizes.size.length; ++i) {
-                    var size = data.sizes.size[i];
-                    if (size.width > jigWd || size.height > jigHt) {
-                        break;
-                    }
-                    prev = size;
-                }
-                size = prev;
-                
-                $('#u').val(size.source);
-                var pElt = $('<p>');
-                var aElt = $('<a>')
-                    .attr('href', 'http://www.flickr.com/photos/' + photo.owner + '/' + photo.id)
-                    .text(photo.title + ' by ' + photo.ownername)
-                    .appendTo(pElt);
-                $('#c').empty().append(pElt);
-                $('#g').removeClass('l');
-                $('#f').addClass('h').removeClass('h');;
-                /*
-                updateSubmit($('#u'));
-                */
-            });
+    var jigHt, jigWd;
+    
+    // Called once we know the image and its dimensions.
+    var showJigsaw = function (src, wd, ht) {
+        if (wd > jigWd) {
+            ht *= jigWd / wd;
+            wd = jigWd;
+        }
+        if (ht > jigHt) {
+            wd *= jigHt / ht;
+            ht = jigHt;
+        }
+        var n = $('#d').val();
+        var args = {
+            u: src,
+            wd: wd,
+            ht: ht,
+            nh: Math.ceil(Math.pow(n * wd / ht, .5)),
+            nv: Math.ceil(Math.pow(n * ht / wd, .5))
+        }
+        var embed = $('<embed>').attr({
+            src: 'jigsaw.svg' + escapeArgs(args),
+            type: 'image/svg+xml',
+            width: jigWd,
+            height: jigHt
         });
+        $('#x').empty().append(embed);
+        $('body').addClass('j');
+        $('#f').removeClass('l');
+    }
+    
+    // User has clicked on the Jigsaw button.
+    $('#f').submit(function (evt) {
+        $(this).addClass('l');
+        var em = $('#a a').height(); // SO TEMPTING to just write 13 here and be done with it.
+        jigWd = $(document).width();
+        jigHt = $(document).height() - 6 * em;
+        
+        if ($(this).hasClass('u')) {
+            // User has specified picture URL directly.
+            // We must loads the image to discover its dimensions.   
+                     
+            var src = $('#u').val();
+            var im = $('<img>').attr('src', src);
+            imElt = im.get(0);
+            var isLoaded = false;
+            im.load(function () {
+                if (!isLoaded) { 
+                    showJigsaw(src, imElt.width, imElt.height);
+                    isLoaded = true;                
+                }
+            });
+            if (imElt.complete) {
+                im.trigger('load');
+            }
+        } else {
+            // User has supplied Flickr tags; use API to find image src and dimens.
+            
+            var tags = $('#t').val();
+            var args = {
+                tags: tags.split().join(','),
+                license: '1,2,4,5,7', // Licences that permit modified works.
+                sort: 'interestingness-desc',
+                content_type: 1, // Still images only
+                media: 'photos',
+                per_page: 25,
+                extras: 'owner_name'
+            };
+            flickrCall('photos.search', args, function (data) {
+                // We have a lost of photos. Choose one at random.
+                var photoIndex = Math.floor(Math.random() * data.photos.photo.length);
+                var photo = data.photos.photo[photoIndex];
+                
+                // Now to find the size of the photo that fits best.
+                flickrCall('photos.getSizes', {photo_id: photo.id}, function (data) {
+                    // Loop over sizes to get best fit.
+                    var prev;
+                    for (var i = 0; i < data.sizes.size.length; ++i) {
+                        var size = data.sizes.size[i];
+                        if (size.width > jigWd || size.height > jigHt) {
+                            break;
+                        }
+                        prev = size;
+                    }
+                    size = prev;
+            
+                    var pElt = $('<p>');
+                    var aElt = $('<a>')
+                        .attr('href', 'http://www.flickr.com/photos/' + photo.owner + '/' + photo.id)
+                        .text(photo.title + ' by ' + photo.ownername)
+                        .appendTo(pElt);
+                    $('#c').empty().append(pElt);
+                    
+                    showJigsaw(size.source, size.width, size.height);
+                });
+            });                
+        }
         evt.preventDefault();
+    });
+    
+    // This supplies the mechanism for switching between modes.
+    $('form a').click(function (evt) {
+        var which = this.parentNode.id == 'dt' ? 'u' : 't';
+        this.parentNode.parentNode.className = which;
     });
 });
